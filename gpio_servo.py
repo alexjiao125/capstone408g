18b3d2c927372c2d2f773c983b88f05f65f2c922
Matt <mflaher@terpmail.umd.edu>
2019-05-13 21:35:27 -0500

    new changes to gpio_servo.py

--- gpio_servo.py
+++ gpio_servo.py
@@ -1,20 +1,22 @@
 #uses pigpio library for software pwm
-from time import sleep
+from time import sleep, time
 import pigpio as pg
 
 #all units in the next 9 lines are in degrees
 scaleAngle = 0.96
-minErr = 5 
-eXPrev = 0
-eYPrev = 0
+minErr = 2 
+maxRotation = 0.6
+eXPrev = 1e-12 
+eYPrev = 1e-12 
 eXSum = 0
 eYSum = 0
-KPx = 0.2
-KPy = 0.05 
-KDx = 0.2
-KDy = 0.03
-KIx = 0.01
-KIy = 0.0
+KPx =0.02 #0.1 
+KPy =0.01 #0.05  
+KDx =0.014 #0.0 
+KDy =0.006  #0.0
+KIx =0.012 #0.0 
+KIy =0.006 #0.0 
+lastTime = 0
 
 angleX = 0
 angleY = 0
@@ -22,8 +24,8 @@ angleY = 0
 #These are the limits of the gimbal to protect the ribbon cable and servo
 #from damage. Last updated: 5/8/19
 xMin = 30
-xMax = 140
-yMin = 80
+xMax = 155
+yMin = 90
 yMax = 160
 
 #must initialize these two variables from python script.
@@ -32,11 +34,11 @@ xtilt=17
 ytilt=27
 
 def initGPIO():
-    global xtilt, ytilt, angleX, angleY
+    global xtilt, ytilt, angleX, angleY, lastTime
     try:
         pi = pg.pi()
-        angleX = 80
-        angleY = 135 
+        angleX = 95 
+        angleY = 95 
         setServoAngle(pi, xtilt, angleX)
         setServoAngle(pi, ytilt, angleY)
         return pi
@@ -70,9 +72,16 @@ def setServoAngle(pi, pin, angle):
 	
 def updateAnglePID(servo, eX, eY):
     assert xtilt > 0 and ytilt > 0, "ensure xtilt, ytilt have been set in gpio_servo.py module"
-    global eXPrev, eYPrev, eXSum, eYSum, angleX, angleY
+    global eXPrev, eYPrev, eXSum, eYSum, angleX, angleY, lastTime
     if eX > minErr or eX < -minErr:
-        angleX -= ((eX * KPx) + (eXPrev * KDx) + (eXSum * KIx))*scaleAngle
+        Xchange = ((eX * KPx) + (eXPrev * KDx) + (eXSum * KIx))*scaleAngle
+        #we don't want any particular adjustment to be too large, or the image becomes blurry and MOSSE will lose the person.
+        #if the amount of error accumulated ( = Xchange) is above the threshold, only adjust the angle by the max threshold
+        if(abs(Xchange)>maxRotation): #
+            angleX -= maxRotation*((Xchange>0)*2-1)
+        else:
+            angleX -= Xchange
+        #print("Angle error X: ", (eX * KPx)*scaleAngle , (eXPrev * KDx)*scaleAngle , (eXSum * KIx)*scaleAngle)
         if angleX < xMin:
             angleX = xMin
         if angleX > xMax:
@@ -81,7 +90,11 @@ def updateAnglePID(servo, eX, eY):
         eXPrev = 0
         eXSum = 0
     if eY > minErr or eY < -minErr:
-        angleY += ((eY * KPy) + (eYPrev * KDy) + (eYSum * KIy))*scaleAngle
+        Ychange = ((eY * KPy) + (eYPrev * KDy) + (eYSum * KIy))*scaleAngle
+        if(abs(Ychange)>maxRotation):
+            angleY += maxRotation*((Ychange>0)*2-1)
+        else:
+            angleY += Ychange
         if angleY < yMin:
             angleY = yMin
         if angleY > yMax:
@@ -95,9 +108,10 @@ def updateAnglePID(servo, eX, eY):
         setServoAngle(servo, ytilt, angleY)
     eXPrev = eX
     eYPrev = eY
-    eXSum += eX
-    eYSum += eY
-    print("angleX: ", angleX, " angleY: ", angleY)
+    eXSum += eX*(time()-lastTime)
+    eYSum += eY*(time()-lastTime)
+    lastTime = time()
+    #print("angleX: ", angleX, " angleY: ", angleY)
 
 if __name__ == '__main__':
 	import sys
